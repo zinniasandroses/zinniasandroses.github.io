2fac5ec3c0c42ac7c2a92b7aa5efe9eb0c8c7ae6
ava <nievescrew2@gmail.com>
2026-06-23 18:24:51 -0400

    lelelele

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function changeText() {
     document.getElementById("test").textContent = "how dare you click that button you evil person";
 }
+let arim = 5;
+let cinat = 1;
+
+let money = 0;
+
+function updateDisplay() {
+  document.getElementById("moneyDisplay").textContent = money;
+}
+
+document.getElementById("addMoneyButton").addEventListener("click", function() {
+  money += 10;
+  updateDisplay();
+});
+
+// Show the starting value when the page loads
+updateDisplay();
